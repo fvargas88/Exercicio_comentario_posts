ae209e53ed9029fc9adf16924a0298c95305d445
fvargas88 <fvargas_88@hotmail.com>
2022-12-23 06:33:39 -0300

    Exercício comntários em postagens

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import entities.Comment;
+import entities.Post;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Comment c1 = new Comment("Tenha uma boa viagem!");
+		Comment c2 = new Comment("Wow que legal!");
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), "Viajando para Nova Zelandia", 
+				"Vou visitar esse país incrível!", 12);
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		Comment c3 = new Comment("Boa noite!");
+		Comment c4 = new Comment("Posso ir estar com você");
+		Post p2 = new Post(sdf.parse("28/07/2018 23:14:19"), "Boa noite pessoal", "Vejo vocês amanhã", 5);
+		p2.addComment(c3);
+		p2.addComment(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Comentarios_postagens {
+}
